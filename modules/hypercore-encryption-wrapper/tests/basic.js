7bae9d6e61d654feaba0a9646d0830376f59202d
fsteff <fsteff@gmx.at>
2020-09-20 20:39:39 +0200

    more tests

--- modules/hypercore-encryption-wrapper/tests/basic.js
+++ modules/hypercore-encryption-wrapper/tests/basic.js
@@ -2,6 +2,7 @@ const hypercore = require('hypercore')
 const wrap = require('../index')
 const tape = require('tape')
 const ram = require('random-access-memory')
+const crypto = require('../../crypto/lib/primitives')
 
 function replicate (a, b, opts) {
   var stream = a.replicate(true, opts)
@@ -52,8 +53,8 @@ tape('basic', t => {
   }
 })
 
-tape('writeStream', t => {
-  t.plan(7)
+tape('stream', t => {
+  t.plan(10)
   const core = wrap(hypercore(ram, null, { valueEncoding: 'utf-8' }), encrypt, decrypt)
   core.on('ready', append)
 
@@ -80,6 +81,16 @@ tape('writeStream', t => {
     core.get(2, (err, data) => {
       t.error(err)
       t.same(data, 'world')
+    })
+
+    const stream = core.createReadStream()
+    let i = 0
+    stream.on('data', (data) => {
+      if (i === 0) t.same(data, 'hello')
+      else if (i === 1) t.same(data, ' ')
+      else if (i === 2) t.same(data, 'world')
+      else t.fail()
+      i++
     })
   }
 })
@@ -114,6 +125,38 @@ tape('replicate', t => {
         t.error(err)
         t.same(data, 'world')
       })
+    })
+  }
+})
+
+tape('crypto', t => {
+  t.plan(7)
+  const key = crypto.generateEncryptionKey()
+  const core = wrap(hypercore(ram, null, { valueEncoding: 'utf-8' }),
+    (data, idx) => crypto.encryptBlockStream(data, idx, key),
+    (data, idx) => crypto.decryptBlockStream(data, idx, key))
+  core.on('ready', append)
+
+  function append () {
+    core.append('hello')
+    core.append([' ', 'world'], read)
+  }
+
+  function read () {
+    t.same(core.length, 3)
+    core.get(0, (err, data) => {
+      t.error(err)
+      t.same(data, 'hello')
+    })
+
+    core.get(1, (err, data) => {
+      t.error(err)
+      t.same(data, ' ')
+    })
+
+    core.get(2, (err, data) => {
+      t.error(err)
+      t.same(data, 'world')
     })
   }
 })
